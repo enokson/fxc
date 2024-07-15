ad94d5498436fb3c85dd9ab9b078310fa93627cb
Joshua Enokson <joshua@enokson.com>
2024-07-15 09:58:14 +0500

    added mkShell and README

---
+++ README.md
@@ -0,0 +1,2 @@
+# FXC
+A simple program to fetch the "offical" exchange rate (F.X.) for the Tojikistan Сомони (Somoni, TJS) and USD.
--- flake.nix
+++ flake.nix
@@ -1,19 +1,30 @@
 {
-  description = "A simple program to get the only 'true' source the forex exchange (fx) rate bewteen the USD and Сомони (Tajikistan Somoni.)";
+  description = "A simple program to get the only 'offical' source the forex exchange (fx) rate bewteen the USD and Сомони (Tajikistan Somoni.)";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    packages.x86_64-linux.fxc = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation {
-      name = "fxc";
-      src = ./src;
-      installPhase = ''
-        mkdir -p $out/bin
-        cp $src/main.sh $out/bin/fxc
-      '';
+  outputs = { self, nixpkgs, ... }@inputs: 
+  let 
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages."x86_64-linux".fxc = pkgs.stdenv.mkDerivation {
+        name = "fxc";
+        src = ./src;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $src/main.sh $out/bin/fxc
+        '';
     };
-    packages.x86_64-linux.default = self.packages.x86_64-linux.fxc;
+    packages."x86_64-linux".default = self.packages."x86_64-linux".fxc;
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rust-analyzer # LSP Server
+        rustfmt       # Formatter
+        clippy        # Linter
+      ];
+    };
   };
 }
